ef77d0f6030cf26e63273dc07ca96def8efc86eb
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2024-01-02 14:26:30 +0100

    Remove useless reg in HPC1

--- gadget_library/MSK/MSKand_hpc1.v
+++ gadget_library/MSK/MSKand_hpc1.v
@@ -23,7 +23,7 @@ module MSKand_hpc1 #(parameter d=`DEFAULTSHARES) (ina, inb, rnd, clk, out);
 (* fv_type = "sharing", fv_latency = 1 *) input  [d-1:0] inb;
 (* fv_type = "sharing", fv_latency = 3 *) output [d-1:0] out;
 (* fv_type = "clock" *) input clk;
-(* fv_type = "random", fv_count=1, fv_rnd_lat_0=0, fv_rnd_count_0=hpc1rnd *)
+(* fv_type = "random", fv_count=1, fv_rnd_lat_0=0, fv_rnd_count_0=ref_n_rnd, fv_rnd_lat_1=2, fv_rnd_count=dom_rnd *)
 input [hpc1rnd-1:0] rnd;
 
 wire [d-1:0] inb_ref;
@@ -31,23 +31,9 @@ wire [d-1:0] inb_ref;
 wire [ref_n_rnd-1:0] rnd_ref;
 assign rnd_ref = rnd[ref_n_rnd-1:0];
 
-wire [dom_rnd-1:0] rnd_mul, rnd_mul_delayed;
 assign rnd_mul = rnd[hpc1rnd-1:ref_n_rnd];
 
-genvar i;
-for (i=0; i<=2; i=i+1) begin: delay_mat_rnd
-    reg [dom_rnd-1:0] mat_rnd;
-    if (i==0) begin
-        always @(posedge clk)
-            mat_rnd <= rnd_mul;
-    end else begin
-        always @(posedge clk)
-            mat_rnd <= delay_mat_rnd[i-1].mat_rnd;
-    end
-end
-assign rnd_mul_delayed = delay_mat_rnd[2].mat_rnd;
-
 MSKref_sni #(.d(d)) rfrsh (.in(inb), .clk(clk), .out(inb_ref), .rnd(rnd_ref));
-MSKand_dom #(.d(d)) mul (.ina(ina), .inb(inb_ref), .clk(clk), .rnd(rnd_mul_delayed), .out(out));
+MSKand_dom #(.d(d)) mul (.ina(ina), .inb(inb_ref), .clk(clk), .rnd(rnd_mul), .out(out));
 
 endmodule
