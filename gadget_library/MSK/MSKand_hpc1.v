85647a8998271988e6b01b5ea729ddea065d8568
cmomin <charles.momin@uclouvain.be>
2024-09-30 17:47:10 +0200

    fix and_hpc1 randomness signals

--- gadget_library/MSK/MSKand_hpc1.v
+++ gadget_library/MSK/MSKand_hpc1.v
@@ -18,7 +18,7 @@
 `ifndef DEFAULTSHARES
 `define DEFAULTSHARES 2
 `endif
-module MSKand_hpc1 #(parameter d=`DEFAULTSHARES) (ina, inb, rnd, clk, out);
+module MSKand_hpc1 #(parameter d=`DEFAULTSHARES) (ina, inb, clk, out, rnd_ref, rnd_mul);
 
 `include "MSKand_hpc1.vh"
 
@@ -31,12 +31,6 @@ module MSKand_hpc1 #(parameter d=`DEFAULTSHARES) (ina, inb, rnd, clk, out);
 input [hpc1rnd-1:0] rnd_mul;
 
 wire [d-1:0] inb_ref;
-
-wire [ref_n_rnd-1:0] rnd_ref;
-assign rnd_ref = rnd[ref_n_rnd-1:0];
-
-wire [dom_rnd-1:0] rnd_mul;
-assign rnd_mul = rnd[hpc1rnd-1:ref_n_rnd];
 
 MSKref_sni #(.d(d)) rfrsh (.in(inb), .clk(clk), .out(inb_ref), .rnd(rnd_ref));
 MSKand_dom #(.d(d)) mul (.ina(ina), .inb(inb_ref), .clk(clk), .rnd(rnd_mul), .out(out));
